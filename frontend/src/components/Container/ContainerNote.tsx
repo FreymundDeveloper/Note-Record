6b97793ed888b0e3198356b7ecc583b135232ea3
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-03 21:16:24 -0300

    ContainerNote Color Flow

--- frontend/src/components/Container/ContainerNote.tsx
+++ frontend/src/components/Container/ContainerNote.tsx
@@ -5,34 +5,48 @@ import { ThemeType } from '../../themes/Theme';
 import { getCardColor } from '../../utils/colorUtils';
 
 interface ContainerNoteProps {
-    title: string;
-    theme?: ThemeType;
-    note: string;
+  title: string;
+  theme?: ThemeType;
+  note: string;
 }
 
 export const ContainerNote: React.FC<ContainerNoteProps> = ({ title, theme, note }) => {
+    const noteValue = parseFloat(note);
+
+    let noteColor = '';
+    if (note === '' || isNaN(noteValue)) {
+        noteColor = '#fff';
+        note = '?';
+    }
+    else if (noteValue >= 0 && noteValue < 6) noteColor = '#d81c41';
+    else if (noteValue >= 6 && noteValue < 8) noteColor = '#FFD700';
+    else if (noteValue >= 8 && noteValue <= 10) noteColor = '#ADFF2F';
+    
+
     return (
-        <NoteContainerStyled title={title} theme={theme}>
-            <NoteIcon>X</NoteIcon>
-            <NoteLabel>Nota: {note}</NoteLabel>
+        <NoteContainerStyled title={title} theme={theme} noteColor={noteColor}>
+            <NoteIcon color={noteColor}>X</NoteIcon>
+            <NoteLabel color={noteColor}>Nota: {note}</NoteLabel>
         </NoteContainerStyled>
     );
 };
 
-const NoteContainerStyled = styled.div<{ title: string; theme: ThemeType }>`
-    display: flex;
-    align-items: center;
-    background-color: ${(props) => darken(0.3, getCardColor(props.title, props.theme))};
-    margin-bottom: 0;
-    height: 40px;
+const NoteContainerStyled = styled.div<{ title: string; theme: ThemeType; noteColor: string }>`
+  display: flex;
+  align-items: center;
+  background-color: ${(props) => darken(0.3, getCardColor(props.title, props.theme))};
+  margin-bottom: 0;
+  height: 40px;
 `;
 
-const NoteIcon = styled.span`
-    font-size: 20px;
-    margin-left: 10px;
-    margin-right: 5px;
+const NoteIcon = styled.span<{ color: string }>`
+  font-size: 20px;
+  margin-left: 10px;
+  margin-right: 5px;
+  color: ${(props) => props.color};
 `;
 
-const NoteLabel = styled.div`
-    font-size: 14px;
+const NoteLabel = styled.div<{ color: string }>`
+  font-size: 14px;
+  color: ${(props) => props.color};
 `;

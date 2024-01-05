c9c37a517865567f9e3ec14b68e660ea4e73d7d8
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-05 18:50:12 -0300

    Get Render Correction

--- frontend/src/components/Text/Title.tsx
+++ frontend/src/components/Text/Title.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { convertToNormalText } from '../../utils/textUtis';
+import { convertToNormalText } from '../../utils/textUtils';
 
 interface TitleProps {
   contentTitle: string;
--- frontend/src/utils/routesUtils.tsx
+++ frontend/src/utils/routesUtils.tsx
@@ -1,27 +1,44 @@
-import axios from 'axios';
-
-export const fetchDataForBimester = async (bimester: string) => {
-    try {
-        const bimesterNumber = mapBimesterStringToNumber(bimester);
-        const response = await axios.get(`http://localhost:3001/results?bimester=${bimester}`);
-        const formattedData = {
-            id: bimesterNumber,
-            details: adaptToEnglish(response.data),
-        };
-
-        return formattedData;
-    } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-        return null;
-    }
+export const adaptToEnglish = (dataArray: any[]) => {
+    return reorderData(
+        improveAdaptedData(
+            dataArray.map(data => ({
+            discipline: data.disciplina,
+            createdData: data.criadoEm,
+            note: data.nota,
+        })))
+    );
 };
 
-export const adaptToEnglish = (dataArray: any[]) => {
-    return dataArray.map(data => ({
-        discipline: data.disciplina,
-        createdData: data.criadoEm,
-        note: data.nota,
-    }));
+export const improveAdaptedData = (adaptedDataArray: any[]) => {
+    const emptyDetails = createEmptyDetails();
+
+    for (let i = adaptedDataArray.length - 1; i >= 0; i--) {
+        const adaptedDiscipline = adaptedDataArray[i].discipline;
+
+        const emptyDetailIndex = emptyDetails.findIndex(detail => detail.discipline === adaptedDiscipline);
+
+        if (emptyDetailIndex !== -1) {
+            emptyDetails.splice(emptyDetailIndex, 1);
+        }
+    }
+
+    adaptedDataArray.push(...emptyDetails);
+
+    return adaptedDataArray;
+};
+
+export const reorderData = (dataArray: any[], order: string[] = ['BIOLOGIA', 'ARTES', 'GEOGRAFIA', 'SOCIOLOGIA']) => {
+    const dataMap = new Map(dataArray.map(data => [data.discipline, data]));
+    const reorderedDataArray: any[] = [];
+
+    order.forEach(discipline => {
+        const data = dataMap.get(discipline) || createEmptyDetails().find(detail => detail.discipline === discipline);
+        if (data) {
+            reorderedDataArray.push(data);
+        }
+    });
+
+    return reorderedDataArray;
 };
 
 export const mapBimesterStringToNumber = (bimester: string) => {
